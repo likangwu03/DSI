339c9fd4ceeb548e3bd696a03a7949b36b86fec3
likang03 <likangwu@ucm.es>
2023-03-30 16:12:49 +0200

    "filtros"

---
+++ Trabajo_DSI_G7/Trabajo_DSI_G7/Input/Input.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_DSI_G7.Input
+{
+    internal class Input
+    {
+    }
+}
