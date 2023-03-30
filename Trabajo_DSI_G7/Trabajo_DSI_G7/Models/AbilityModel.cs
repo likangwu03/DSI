e5dc5b8b8f5ef15b466caf8054f4939d680b6d0d
likang03 <likangwu@ucm.es>
2023-03-30 19:50:49 +0200

    cosas

---
+++ Trabajo_DSI_G7/Trabajo_DSI_G7/Models/AbilityModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_DSI_G7.Models
+{
+    public  class Ability
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string imagen { get; set; }
+
+        List<model> models { get; set; }
+    }
+}
---
+++ Trabajo_DSI_G7/Trabajo_DSI_G7/Models/PotionModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_DSI_G7.Models
+{
+    public class PotionModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Imagen { get; set; }
+        //public Image Img;
+        public string Explicacion { get; set; }
+        //public estados Estado { get; set; }
+        public int Energia { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int RX { get; set; }
+        public int RY { get; set; }
+    }
+}
